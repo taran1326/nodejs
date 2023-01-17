12dc09993b466d3f3458a9ec4c0399977d8b4fef
Taran Mittal <taranmittal.1717@gmail.com>
2023-01-17 15:41:36 +0530

    changes

--- codial/controllers/users_controller.js
+++ codial/controllers/users_controller.js
@@ -1,3 +1,56 @@
+const User = require('../models/user')
+
 module.exports.profile = function(req, res){
     return res.end('<h1> User profile </h1>')
+}
+
+
+module.exports.signUp = function(req, res){
+    return res.render('user_sign_up' , {
+        title:'Codial | Sign up'
+    })
+}
+
+module.exports.signIn = function(req, res){
+    return res.render('user_sign_in' , {
+        title:"Codial | Sign in"
+    })
+}
+
+
+module.exports.create = function(req, res){
+
+    if(req.body.password != req.body.confirm_password){
+        return res.redirect('back') ;
+    }
+
+    User.findOne({email : req.body.email}, function(err , user){
+            if(err){
+                console.log("Error in finding the user with given e-mail");
+                return; 
+ 
+            }
+
+            if(!user){
+                User.create(req.body , function(err, user){
+                    if(err){
+                        console.log(err);
+                        return;
+                    }
+
+                    return res.redirect('./sign-in')
+                })
+            
+            }else{
+                
+                return res.redirect('back');
+            }
+        }
+    )
+    //TODO
+}
+
+
+module.exports.createSession = function(req, res){
+    //TODO
 }
--- codial/routes/users.js
+++ codial/routes/users.js
@@ -4,5 +4,9 @@ const router = express.Router();
 const userController =  require('../controllers/users_controller');
 
 router.get('/profile' ,  userController.profile);
+router.get('/sign-up' , userController.signUp); 
+router.get('/sign-in' , userController.signIn);
+
+router.post('/create' , userController.create); 
 
 module.exports = router;
